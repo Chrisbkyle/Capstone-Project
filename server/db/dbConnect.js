3a45bca20970947f9dd58a6e5c760c8420e2c7a5
Chris <christopher.b.kyle@gmail.com>
2023-07-09 22:06:16 +1200

    require dotenv

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(   
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
+    process.env.DB_NAME || 'office_chef', 
+    process.env.DB_USER|| 'root', 
+    process.env.DB_PASSWORD || 'pass', 
     {  
         host: process.env.DB_HOST || db,
         dialect: 'mysql',
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT || 8080
 });
 
 
